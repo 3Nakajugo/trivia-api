36d7b2b7175c5412e2935cb159cb50cfb217fa25
3Nakajugo <edinahmargarita@gmail.com>
2019-11-07 20:22:10 +0300

    add flask cors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -17,15 +17,42 @@ def create_app(test_config=None):
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
 
+  # configure cors
+  CORS(app, resources={r"/*": {"origins": "*"}})
+
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    '''
+    Function to configure cors headers
+    '''
+
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
+    return response  
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+
+  @app.route('/categories')
+  def get_categories():
+    '''
+      Endpoint to get all categories. 
+    '''
+    categories = []
+    results = Category.query.all()
+    for category in results:
+      categories.append(category.type)
+    return jsonify({
+      'categories': categories,
+      'success': True
+    }), 200
+
 
 
   '''
